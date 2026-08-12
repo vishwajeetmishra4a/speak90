1e9c8258bee240f74eb7799666c7d438aaeca715
Mishra <2488303@cognizant.com>
2026-08-12 12:21:58 +0530

    Upload html,CSS,JS files

---
+++ assets/js/app.js
@@ -0,0 +1,45 @@
+const findTopicBtn = document.getElementById("findTopicBtn");
+
+const shuffleBox = document.getElementById("shuffleBox");
+
+const actionButtons = document.getElementById("actionButtons");
+
+let selectedTopic = "";
+
+findTopicBtn.addEventListener("click", () => {
+
+    let count = 0;
+
+    actionButtons.classList.add("hidden");
+
+    const shuffleInterval = setInterval(() => {
+
+        const randomIndex = Math.floor(
+            Math.random() * topics.length
+        );
+
+        shuffleBox.textContent = topics[randomIndex];
+
+        count++;
+
+        if (count >= 25) {
+
+            clearInterval(shuffleInterval);
+
+            selectedTopic = topics[
+                Math.floor(Math.random() * topics.length)
+            ];
+
+            shuffleBox.innerHTML = `
+                <div>
+                    ✅ <br><br>
+                    ${selectedTopic}
+                </div>
+            `;
+
+            actionButtons.classList.remove("hidden");
+        }
+
+    }, 120);
+
+});
---
+++ assets/js/topic.js
@@ -0,0 +1,26 @@
+const topics = [
+
+    "Artificial Intelligence",
+    "Climate Change",
+    "Remote Work",
+    "Leadership",
+    "Blockchain",
+
+    "Cybersecurity",
+    "Space Exploration",
+    "Digital Privacy",
+    "Innovation",
+    "Work Life Balance",
+
+    "Social Media",
+    "Urbanization",
+    "Entrepreneurship",
+    "Renewable Energy",
+    "Smart Cities",
+
+    "Future of Education",
+    "Electric Vehicles",
+    "Robotics",
+    "Cloud Computing",
+    "Internet of Things"
+];
